7a86ee629f0e8f348dc9160b76f0023342502a41
Michał Szajbe <michal.szajbe@gmail.com>
2018-07-09 16:11:13 +0200

    Add middleware directly to app

--- lib/rack/slash_merger/railtie.rb
+++ lib/rack/slash_merger/railtie.rb
@@ -2,7 +2,7 @@ module Rack
   class SlashMerger
     class Railtie < ::Rails::Railtie
       initializer 'slash_merger.insert_middleware' do |app|
-        app.config.middleware.use "Rack::SlashMerger"
+        app.middleware.use "Rack::SlashMerger"
       end
     end
   end
